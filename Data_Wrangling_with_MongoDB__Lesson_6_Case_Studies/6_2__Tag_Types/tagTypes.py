a66c6026f93dcc0455bcd7bc2dc7467374c97531
Ron Rihoo <ron.rihoo@gmail.com>
2015-12-30 23:40:14 -0600

    Required Course Work
    Data Wrangling with MongoDB. Lesson 6. First 3 cases out of 5.

---
+++ Data_Wrangling_with_MongoDB__Lesson_6_Case_Studies/6_2__Tag_Types/tagTypes.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xml.etree.cElementTree as ET
+import pprint
+import re
+
+lower = re.compile(r'^([a-z]|_)*$')
+lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+
+def key_type(element, keys):
+    if element.tag == "tag":
+        if lower.search(element.attrib['k']):
+            keys['lower'] += 1
+        elif lower_colon.search(element.attrib['k']):
+            keys['lower_colon'] += 1
+        elif problemchars.search(element.attrib['k']):
+            keys['problemchars'] += 1
+        else:
+            keys['other'] += 1
+        pass
+        
+    return keys
+
+def process_map(filename):
+    keys = {"lower": 0, "lower_colon": 0, "problemchars": 0, "other": 0}
+    for _, element in ET.iterparse(filename):
+        keys = key_type(element, keys)
+
+    return keys
+
+
+
+def test():
+    keys = process_map('example.osm')
+    pprint.pprint(keys)
+    assert keys == {'lower': 5, 'lower_colon': 0, 'other': 1, 'problemchars': 1}
+
+
+if __name__ == "__main__":
+    test()
